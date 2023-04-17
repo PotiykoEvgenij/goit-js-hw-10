b7f379e2d027e37cb7adfab7c56b0abb336d8de4
PotiykoEvgenij <potijkoevgenij@gmail.com>
2023-04-17 23:20:14 +0300

    hw10

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import { debounce } from 'lodash.debounce';
+import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import { fetchCountries } from './js/fetchCountries';
 
@@ -7,7 +7,7 @@ const DEBOUNCE_DELAY = 300;
 
 const input = document.getElementById('search-box');
 const countryList = document.querySelector('.country-list');
-const countryInfo = documet.querySelector('.country-info');
+const countryInfo = document.querySelector('.country-info');
 
 const handleInput = debounce((event) => {
     const inputValue = event.target.value;
@@ -18,7 +18,16 @@ const handleInput = debounce((event) => {
 
     fetchCountries(inputValue)
         .then((countries) => {
-            renderCountryList(countries);
+            if (countries === null || countries === undefined) {
+                countryList.innerHTML = '';
+                countryInfo.innerHTML = '';
+            } else if (countries.length === 1) {
+                countryList.innerHTML = '';
+                renderCountryInfo(countries[0]);
+            } else if (countries.length !== 1) {
+                countryInfo.innerHTML = '';
+                renderCountryList(countries);                
+            }
         })
         .catch((error) => {
             console.log(error);
@@ -33,7 +42,7 @@ function renderCountryList(countries) {
     countries.forEach((country) => {
         const listItem = document.createElement('li');
 
-        listItem.innerText = country.name.official;
+        listItem.innerHTML = `<img src="${country.flags.png}" alt="${country.name.common}" class="flag-country"><span>${country.name.common}</span>`;
 
         listItem.addEventListener('click', () => {
             renderCountryInfo(country);
@@ -43,12 +52,16 @@ function renderCountryList(countries) {
 };
 
 function renderCountryInfo(country) {
-    const languages = country.languages.map((lang) => lang.name).join(', ');
+    const countryLanguages = country.languages;
+    const languages = [];
+    Object.keys(countryLanguages).forEach(key => {
+        const value = countryLanguages[key];
+        languages.push(value)
+    });
 
     countryInfo.innerHTML =
-    `<img src="${country.flags.png}" alt="${country.name.common}">
-    <h2>${country.name.common}</h2>
+    `<h2><img src="${country.flags.png}" alt="${country.name.common}">${country.name.common}</h2>
     <p><strong>Capital:</strong> ${country.capital[0]}</p>
     <p><strong>Population:</strong> ${country.population}</p>
-    <p><strong>Languages:</strong> ${Object.values(country.languages).join(', ')}</p>`;
+    <p><strong>Languages:</strong> ${languages.join(', ')}</p>`;
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -28,20 +28,3 @@ const fetchCountries = (name) => {
 };
 
 export { fetchCountries };
-
-// const fetchCountries = (name) => {
-//     return fetch('https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages')
-//         .then((response) => {
-//             if (!response.ok) {
-//                 throw new Error('Network response was not ok');
-//             }
-//             return response.json();
-//         })
-//         .catch((error) => {
-//             console.error('Oops, there is no country with that name');
-//         });
-// };
-
-// export default { fetchCountries };
-
-// https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages
