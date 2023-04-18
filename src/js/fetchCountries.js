637085d48d995497415643c4d5a93e8476dc5dcc
PotiykoEvgenij <potijkoevgenij@gmail.com>
2023-04-18 21:03:59 +0300

    hw10

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -8,21 +8,6 @@ const fetchCountries = (name) => {
       }
       return response.json();
     })
-    // .then((data) => {
-    //   if (data.length > 10) {
-    //     Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-    //   } else if (data.length >= 2 && data.length <= 10) {
-    //     return data;
-    //   } else if (data.length === 1) {
-    //     return data;
-    //   } else {
-    //     Notiflix.Notify.failure('Oops, there is no country with that name');
-    //   }
-    // })
-    // .catch((error) => {
-    //   console.error('Error fetching countries:', error);
-    //   Notiflix.Notify.failure('Oops, there is an error. Please try again later.');
-    // });
 };
 
 export { fetchCountries };
